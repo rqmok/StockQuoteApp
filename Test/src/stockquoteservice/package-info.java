de70313c706dd319daf8aa060ea05ca064fe8eca
Zeeshan Khan <zkha0002@student.monash.edu>
2018-05-02 22:13:09 +1000

    Revert "Re-setup initial project."
    This reverts commit d5a71a91876bf9265e23802691f006866288a1c1.

---
+++ Test/src/stockquoteservice/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * 		Please Type your service description here
+ * 	
+ * 
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://StockQuoteService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package stockquoteservice;
