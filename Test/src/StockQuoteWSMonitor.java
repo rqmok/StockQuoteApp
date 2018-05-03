1e8f590b96ffc443b7a793cad304bbc684a6515c
Tharusha Wickramasinghe <tnwic1@student.monash.edu>
2018-05-04 00:58:29 +1000

    added private variables and constructor to StockQuoteWSMonitor class

--- Test/src/StockQuoteWSMonitor.java
+++ Test/src/StockQuoteWSMonitor.java
@@ -1,7 +1,23 @@
-import javafx.beans.property.SimpleStringProperty;
 
+import javafx.beans.property.SimpleStringProperty;
 
 public class StockQuoteWSMonitor extends Monitor{
 
 
+    private SimpleStringProperty symbolLabel;
+    private SimpleStringProperty lastTradeLabel;
+    private SimpleStringProperty dateLabel;
+    private SimpleStringProperty timeLabel;
+
+    public monitorTypes monitorType = monitorTypes.STOCK_QUOTE_WS_MONITOR;
+
+    StockQuoteWSMonitor(SimpleStringProperty symbol, SimpleStringProperty lastTrade, SimpleStringProperty date, SimpleStringProperty time){
+
+        this.symbolLabel = symbol;
+        this.lastTradeLabel =lastTrade;
+        this.dateLabel = date;
+        this.timeLabel = time;
+
+    }
+    
 }
