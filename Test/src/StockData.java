e15ae820fd4696182f34adebeddcec7c85488469
Zeeshan Khan <zkha0002@student.monash.edu>
2018-05-03 22:36:16 +1000

    design-doc: Change lastTrade from float to String.

--- Test/src/StockData.java
+++ Test/src/StockData.java
@@ -2,22 +2,15 @@
 public class StockData {
 
     private String symbol;
-    private float lastTrade;
+    private String lastTrade;
     private String date;
     private String time;
 
-    public StockData(String symbol, float lastTrade, String date, String time) {
+    public StockData(String symbol, String lastTrade, String date, String time) {
         // Check no null values
-        if (symbol == null || date == null || time == null) {
+        if (symbol == null || lastTrade == null || date == null || time == null) {
             // Print message and do not create an object
             System.console().printf("StockData: Null value provided in constructor.");
-            return;
-        }
-
-        // Check float value
-        if (lastTrade < 0) {
-            // Print message and do not create an object
-            System.console().printf("StockData: lastTrade is negative.");
             return;
         }
 
@@ -29,7 +22,7 @@ public class StockData {
     }
 
     public String getSymbol() { return this.symbol; }
-    public float getLastTrade() { return this.lastTrade; }
+    public String getLastTrade() { return this.lastTrade; }
     public String getDate() { return this.date; }
     public String getTime() { return this.time; }
 
