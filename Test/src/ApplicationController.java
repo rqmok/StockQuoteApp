735248675a629b207a1ff609adae45f644b19be4
Tharusha Wickramasinghe <tnwic1@student.monash.edu>
2018-05-05 16:33:32 +1000

    initialised variables in the application class

--- Test/src/ApplicationController.java
+++ Test/src/ApplicationController.java
@@ -1,11 +1,34 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
 
 public class ApplicationController extends Application {
+
+    Stage window;
+    TableView<StockData> mainTable;
+    //declare new variable of type TextField. This is what we will provide users to input data
+    TextField stockSymbol;
+    //List of stocks
+    Stock[] stocks;
 
     public static void main(String[] args){
         launch(args);
     }
-    
 
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("Stock Tracker App");
+    }
 }
