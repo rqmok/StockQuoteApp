14d81472b33e680f572086f3c55b42e5dedeebb8
Tharusha Wickramasinghe <tnwic1@student.monash.edu>
2018-05-05 16:23:28 +1000

    Created javafx application class -currently contains nothing

---
+++ Test/src/ApplicationController.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+
+public class ApplicationController extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+
+
+}
