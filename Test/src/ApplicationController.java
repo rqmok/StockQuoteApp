99a5319220e21b6d408d9b8f7c9300721e2abdd4
Tharusha Wickramasinghe <tnwic1@student.monash.edu>
2018-05-05 16:44:38 +1000

    Added table columns in  ApplicationController class

--- Test/src/ApplicationController.java
+++ Test/src/ApplicationController.java
@@ -30,6 +30,39 @@ public class ApplicationController extends Application {
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         window.setTitle("Stock Tracker App");
+
+        //Making Columns
+        //This column is for the symbol label
+        TableColumn<StockData, String> symbolColumn = new TableColumn<>("Stock Symbol");
+        symbolColumn.setMinWidth(200);
+        //Makes it take the column value from the monitor variables
+        symbolColumn.setCellValueFactory(new PropertyValueFactory<>("symbolLabel"));
+
+        //This column is for the last trade label
+        TableColumn<StockData, String> tradeColumn = new TableColumn<>("Trade Symbol");
+        tradeColumn.setMinWidth(200);
+        //Makes it take the column value from the monitor variables
+        tradeColumn.setCellValueFactory(new PropertyValueFactory<>("lastTradeLabel"));
+
+        //This column is for the date label
+        TableColumn<StockData, String> dateColumn = new TableColumn<>("Date");
+        dateColumn.setMinWidth(200);
+        //Makes it take the column name from the monitor variables
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("dateLabel"));
+
+        //This column is for the time label
+        TableColumn<StockData, String> timeColumn = new TableColumn<>("Time");
+        timeColumn.setMinWidth(200);
+        //Makes it take the column name from the monitor variables
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("timeLabel"));
+
+        mainTable = new TableView<>();
+        //feed our observable list into the table
+        mainTable.setItems(getAllStockData());
+        //Put the columns into our table
+        mainTable.getColumns().addAll(symbolColumn,tradeColumn,dateColumn,timeColumn);
+
+
     }
 
 
