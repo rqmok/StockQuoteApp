d5dd9b1f01a4bda138b681b40a92abd8573f3202
Zeeshan Khan <zkha0002@student.monash.edu>
2018-05-19 17:00:37 +1000

    src: Run separate updaters for each service.

--- App/StockQuoteApp/src/StockUpdaterClock.java
+++ App/StockQuoteApp/src/StockUpdaterClock.java
@@ -5,15 +5,18 @@ public class StockUpdaterClock implements Runnable {
     // Interval for update stock data
     private long intervalInSeconds;
 
-    public StockUpdaterClock(UpdateStockDataDelegate updateStockDataDelegate, long intervalInSeconds) {
+    // The stock service that needs to be updated
+    private StockService.serviceTypes serviceType;
+
+    public StockUpdaterClock(UpdateStockDataDelegate updateStockDataDelegate, long intervalInSeconds, StockService.serviceTypes serviceType) {
         this.updateStockDataDelegate = updateStockDataDelegate;
         this.intervalInSeconds = intervalInSeconds;
-
+        this.serviceType = serviceType;
     }
 
     public void run() {
         while (true) {
-            updateStockDataDelegate.updateStockData();
+            updateStockDataDelegate.updateStockData(serviceType);
 
             try {
                 Thread.sleep(intervalInSeconds * 1000);
